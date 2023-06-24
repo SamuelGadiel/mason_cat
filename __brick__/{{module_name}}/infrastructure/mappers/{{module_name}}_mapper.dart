c0b6efb1fe7bcda33c8d2367377796c5d40bc44f
SamuelGadiel <samuelgadiel@gmail.com>
2023-06-24 01:16:37 -0300

    create infrastructure mappers

---
+++ __brick__/{{module_name}}/infrastructure/mappers/{{module_name}}_mapper.dart
@@ -0,0 +1,9 @@
+import '../../domain/entities/{{module_name}}.dart';
+
+class {{module_name.pascalCase()}}Mapper {
+  static {{module_name.pascalCase()}} fromJson(Map<String, dynamic> json){
+    return {{module_name.pascalCase()}}(
+
+    );
+  }
+}
