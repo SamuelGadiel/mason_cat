00afe589ec7313cda08045160b57238ac1959c40
SamuelGadiel <samuelgadiel@gmail.com>
2023-06-24 01:08:02 -0300

    create domain usecase

---
+++ __brick__/{{module_name}}/domain/usecases/get_{{module_name}}.dart
@@ -0,0 +1,18 @@
+import '../entities/{{module_name}}.dart';
+{{#has_parameters}}import '../entities/{{> parameters.snakeCase() }}.dart';{{/has_parameters}}
+import '../repositories/{{module_name}}_repository.dart';
+
+abstract class Get{{module_name.pascalCase()}} {
+  Future<Either<Failure, {{module_name.pascalCase()}}>> call({{#has_parameters}}{{> parameters }} parameters{{/has_parameters}});
+}
+
+class Get{{module_name.pascalCase()}}Implementation implements Get{{module_name.pascalCase()}} {
+  final {{module_name.pascalCase()}}Repository repository;
+
+  Get{{module_name.pascalCase()}}Implementation(this.repository);
+
+  @override
+  Future<Either<Failure, {{module_name.pascalCase()}}>> call({{#has_parameters}}{{> parameters }} parameters{{/has_parameters}}) async {
+    return repository();
+  }
+}
